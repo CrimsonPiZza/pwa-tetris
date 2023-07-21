030de986d9cd731505a4bc2c762461ba7c0be469
Hourn Sovannara <hournsovannara18@kit.edu.kh>
2023-07-21 22:32:47 +0900

    pwa-5

---
+++ docs/precache-manifest.7319fb67687de53f58348a2eca5e4db3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "50d245e34ebf652d10a0",
+    "url": "/css/app.701a0f55.css"
+  },
+  {
+    "revision": "50d245e34ebf652d10a0",
+    "url": "/js/app.292274de.js"
+  },
+  {
+    "revision": "8ce62700fc70942b196d",
+    "url": "/js/chunk-vendors.07d38a7d.js"
+  },
+  {
+    "revision": "1b869e5bbe035a641f4885cf89288263",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7adeb24d698780e3b0b009421cdb0ea6",
+    "url": "/loader.css"
+  },
+  {
+    "revision": "75c4bc1869b797ad8eb8b122cce22dcc",
+    "url": "/music.mp3"
+  },
+  {
+    "revision": "7e27bb068ff021ccb363f7a8a7b149be",
+    "url": "/qr.jpeg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+];
