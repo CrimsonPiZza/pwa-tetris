3043f738f89684ddcea2f1db8668e55de9a22e56
Hourn Sovannara <hournsovannara18@kit.edu.kh>
2023-07-21 22:06:07 +0900

    dist

---
+++ dist/precache-manifest.c93e4075b39bd42acdc847909f4495c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb2cc45807e942512cbd",
+    "url": "/pwa-tetris/css/app.322ed4f6.css"
+  },
+  {
+    "revision": "bb2cc45807e942512cbd",
+    "url": "/pwa-tetris/js/app.d0760995.js"
+  },
+  {
+    "revision": "8ce62700fc70942b196d",
+    "url": "/pwa-tetris/js/chunk-vendors.07d38a7d.js"
+  },
+  {
+    "revision": "f73da797b27667ffe3ecb40b963f4e47",
+    "url": "/pwa-tetris/index.html"
+  },
+  {
+    "revision": "7adeb24d698780e3b0b009421cdb0ea6",
+    "url": "/pwa-tetris/loader.css"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/pwa-tetris/robots.txt"
+  },
+  {
+    "revision": "75c4bc1869b797ad8eb8b122cce22dcc",
+    "url": "/pwa-tetris/music.mp3"
+  },
+  {
+    "revision": "7e27bb068ff021ccb363f7a8a7b149be",
+    "url": "/pwa-tetris/qr.jpeg"
+  }
+];
